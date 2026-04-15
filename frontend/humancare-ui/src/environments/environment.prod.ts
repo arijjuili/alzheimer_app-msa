61fc9cc260749ab24baad976dc3c30c5bff89e83
HebhebJ <iheb.jlassi@esprit.tn>
2026-04-15 05:22:09 +0100

    fix: set prod env to DigitalOcean IP, add vercel.json for Angular routing

--- frontend/humancare-ui/src/environments/environment.prod.ts
+++ frontend/humancare-ui/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8081',
+  apiUrl: 'http://134.122.73.7:8081',
   keycloak: {
-    url: 'http://localhost:8090',
+    url: 'http://134.122.73.7:8090',
     realm: 'humancare',
     clientId: 'humancare-webapp'
   }
